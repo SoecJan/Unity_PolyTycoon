79c1901525b71456d2439296359195b49207c07c
SoecJan <jan.soechtig@gmail.com>
2018-11-04 18:05:56 +0100

    Test 4
    Test 4: Scene changes

--- Assets/Game/Scripts/Vehicle/Mover.cs
+++ Assets/Game/Scripts/Vehicle/Mover.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class Mover : MonoBehaviour {
+public class Mover : MonoBehaviour
+{
+	[SerializeField] private float speed;
+	[SerializeField] private Vector3 targetPoint;
 
 	// Use this for initialization
 	void Start () {
-		
+		transform.LookAt(targetPoint);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		Vector3 difference = targetPoint - transform.position;
+		transform.position = transform.position + (speed * Time.deltaTime * difference.normalized);
 	}
 }
