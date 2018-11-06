64c15ba04e3af5637c5f801f9bd70c15fae8e487
keymini <jan.soechtig@gmail.com>
2018-11-06 22:05:43 +0100

    First Terrain Implementation
    First Terrain Implementation of the terrain generation

--- Assets/Menue/Scripts/SingleplayerMenu.cs
+++ Assets/Menue/Scripts/SingleplayerMenu.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SingleplayerMenu : AbstractUi 
 {
 	[Header("Navigation")]
 	[SerializeField] private Button _backButton;
+	[SerializeField] private Button _playButton;
 
 	private PlayMenue _playMenu;
 
@@ -14,12 +16,30 @@ public class SingleplayerMenu : AbstractUi
 	void Start () {
 		_playMenu = FindObjectOfType<PlayMenue>();
 		_backButton.onClick.AddListener(OnBackClick);
+		_playButton.onClick.AddListener(OnPlayClick);
 	}
 
 	private void OnBackClick()
 	{
 		SetVisible(false);
 		_playMenu.SetVisible(true);
+	}
+
+	private void OnPlayClick()
+	{
+		Debug.Log("Play!");
+		StartCoroutine(LoadScene(1));
+	}
+
+	private IEnumerator LoadScene(int scene)
+	{
+		AsyncOperation operation = SceneManager.LoadSceneAsync(scene);
+		while (!operation.isDone)
+		{
+			yield return null;
+		}
+
+		SceneManager.UnloadSceneAsync("MainMenu");
 	}
 
 	public override void Reset()
