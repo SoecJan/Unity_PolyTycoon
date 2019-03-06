96ab8cebe29313596734ee2d569f9a9d8ac34503
Jan-Hendrik Soechtig <jan-hendrik.soechtig@uni-oldenburg.de>
2019-03-06 23:01:17 +0100

    Added first iteration of PreGameMenu.unity UI with animations.

---
+++ Assets/PreGameMenue/Scripts/AbstractUi.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractUi : MonoBehaviour
+{
+	[SerializeField] private GameObject _visibleObject;
+
+	public GameObject VisibleObject {
+		get {
+			return _visibleObject;
+		}
+
+		set {
+			_visibleObject = value;
+		}
+	}
+
+	public virtual void SetVisible(bool visible)
+	{
+		OnVisibilityChange(visible);
+		Animator anim = GetComponentInChildren<Animator>();
+		if (visible)
+		{
+			VisibleObject.SetActive(visible);
+		}
+		else
+		{
+			
+			if (anim)
+			{
+				StartCoroutine(TransitionEndAwait(anim));
+			}
+			else
+			{
+				gameObject.SetActive(false);
+			}
+		}
+	}
+
+	private IEnumerator TransitionEndAwait(Animator animator)
+	{
+		animator.SetTrigger("Transition");
+		while (animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1f)
+		{
+			Debug.Log(animator.GetCurrentAnimatorStateInfo(0).normalizedTime);
+			yield return null;
+		}
+		Debug.Log("Inactive");
+		animator.gameObject.SetActive(false);
+	}
+
+	public abstract void Reset();
+
+	protected abstract void OnVisibilityChange(bool visible);
+}
---
+++ Assets/PreGameMenue/Scripts/AbstractUiStateMachineBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbstractUiStateMachineBehaviour : StateMachineBehaviour
+{
+	override public void OnStateExit(Animator animator,
+		AnimatorStateInfo stateInfo,
+		int layerIndex)
+	{
+		if (stateInfo.IsName("ExitAnimation"))
+		{
+			
+		}
+	}
+}
